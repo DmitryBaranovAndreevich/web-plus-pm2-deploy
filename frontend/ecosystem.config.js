777b1a338505feab6623a8f98b6e4a9f32fb3f93
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-03-19 14:39:32 +0300

    add: ecosystem for frontend

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: "https://github.com/DmitryBaranovAndreevich/web-plus-pm2-deploy.git",
+      path: DEPLOY_PATH,
+      "post-deploy": "npm i && npm run build",
+    },
+  },
+};
