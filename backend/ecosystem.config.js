2acec346a8a4de76d258f608e35bb5024e7627d5
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-03-20 08:08:38 +0300

    add: en lang

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = "origin/main",
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "pm2-backend",
+      script: "./backend/dist/app.js",
+    },
+  ],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: "https://github.com/DmitryBaranovAndreevich/mesto-project-plus.git",
+      path: DEPLOY_PATH,
+      "pre-deploy-local": `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      "post-deploy": "npm i && npm run build",
+    },
+  },
+};
