40c2d8edbb8edfc70436bd7d0f11d5fec80492f2
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-03-21 00:43:46 +0300

    update repo

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
       path: DEPLOY_PATH,
       "pre-deploy-local": `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       "post-deploy":
-        "rm -rf frontend && cd backend && npm i &&  npm run build && pm2 reload all",
+        "rm -rf frontend && cd backend && npm i &&  npm run build && pm2 restart all",
     },
   },
 };
