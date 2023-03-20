08be7ec6b1ef6c00046cb9275263fe236bd5ec02
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-03-21 00:47:57 +0300

    update repo

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,13 +18,18 @@ mongoose.connect(DB_ADDRESS);
 app.use(
   cors({
     origin: "*",
-    credentials: true, 
+    credentials: true,
     optionsSuccessStatus: 200,
   })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
 app.use(routes);
 app.use(errors());
 app.use(errorHandler);
