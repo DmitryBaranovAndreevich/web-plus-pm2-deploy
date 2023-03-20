ca2c5c35c0ec6b58e326bd2f2c1a18a53458d1e8
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-03-20 08:50:01 +0300

    update repo

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
       repo: "https://github.com/DmitryBaranovAndreevich/mesto-project-plus.git",
       path: DEPLOY_PATH,
       "pre-deploy-local": `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      "post-deploy": "cd backend && npm i && npm run build",
+      "post-deploy": "cd pm2-backend && npm i && npm run build",
     },
   },
 };
