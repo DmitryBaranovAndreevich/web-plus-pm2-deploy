fddedbdf78cd4f031844c1fea877d93eec386373
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-03-21 17:45:44 +0300

    update repo

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,7 +4,7 @@ const {
   DEPLOY_USER,
   DEPLOY_HOST,
   DEPLOY_PATH,
-  DEPLOY_REF = "origin/master",
+  DEPLOY_REF = "origin/main",
 } = process.env;
 
 module.exports = {
@@ -23,7 +23,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: "https://github.com/DmitryBaranovAndreevich/web-plus-pm2-deploy.git",
       path: DEPLOY_PATH,
-      "pre-deploy-local": `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      "pre-deploy-local": `scp .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       "post-deploy":
         "rm -rf frontend && cd backend && npm i &&  npm run build && pm2 restart all",
     },
